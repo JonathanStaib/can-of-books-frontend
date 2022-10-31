fb966b8b8314cf60297b92b952989aed7df864fb
JonathanStaib <JonathanStaibb@gmail.com>
2022-10-31 19:36:20 -0400

    completed lab 11

--- src/About.js
+++ src/About.js
@@ -1,11 +1,34 @@
 import { Component } from "react";
+import {Card} from 'react-bootstrap';
+import JonathanImage from './jonathan.jpg';
+import JordanImage from './jordan.jpg';
 
 class Profile extends Component {
 
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    return (
+      <>
+        <main>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img class='jonny' src={JonathanImage} a href=""/>
+            <Card.Body>
+              <Card.Title>Jonathan Staib</Card.Title>
+              <Card.Text>Hello</Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card style={{ width: '18rem' }}>
+            <Card.Img class='jordan' src={JordanImage} a href="https://github.com/nawktopus" />
+            <Card.Body>
+              <Card.Title>Jordan Kwan</Card.Title>
+              <Card.Text>Hello</Card.Text>
+            </Card.Body>
+          </Card>
+        </main>
+      </>
+    );
   }
-};
+}
 
 export default Profile;
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,30 +1,86 @@
 import React from 'react';
+import axios from 'axios';
+import {Carousel, Container,} from 'react-bootstrap';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       books: []
+    };
+  }
+  /* TODO: Make a GET request to your API to fetch all the books from the database  */
+
+  getBooks = async () => {
+    try {
+      let bookData = await axios.get(`${process.env.REACT_APP_SERVER}/books`);
+      this.setState({
+        books: bookData.data
+      });
+
+    } catch (error) {
+      console.log('we have an error: ', error.response);
     }
+  };
+
+  componentDidMount() {
+    this.getBooks();
   }
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
   render() {
 
-    /* TODO: render all the books in a Carousel */
+    let carouselItems = this.state.books.map((book, index) => (
+      <Carousel.Item key={index}>
+        <h1>Name: {book.title}</h1>
+        <p>Info: {book.description}</p>
+        <p>Status: {book.status}</p>
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+    ));
+
 
     return (
-      <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
-      </>
-    )
+    // <>
+    //   <header>
+    //     <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+    //   </header>
+    //   <main>
+    //     {
+    //       this.state.books.length > 0 &&
+    //       <>
+    //         {books}
+    //       </>
+    //     }
+    //   </main>
+    // </>
+
+      this.state.books.length > 0 ? (
+
+        <>
+          <h1>Books!</h1>
+          <Container>
+
+          </Container>
+          {
+            this.state.books.length > 0 &&
+            <>
+              <Container bg="dark" >
+                <Carousel bg="dark" variant="dark" showControls showIndicators>
+                  {carouselItems}
+                </Carousel>
+              </Container>
+            </>
+          }
+        </>
+      )
+        :
+        (<p>Book Collection is Empty</p>)
+
+
+    );
   }
 }
 
